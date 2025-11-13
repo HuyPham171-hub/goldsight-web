2e6074e27035f6e87f4fd7f99eec63bf8f22ded0
Huy Pham <124356254+HuyPham171-hub@users.noreply.github.com>
2025-11-13 17:31:15 +0700

    Optimize for Render 512MB: remove plugins, simplify config

--- rxconfig.py
+++ rxconfig.py
@@ -1,16 +1,8 @@
 import reflex as rx
 import os
 
-# Detect if running in production (Render) or local
-is_prod = os.getenv("RENDER") is not None
-
 config = rx.Config(
     app_name="goldsight",
-    backend_host="0.0.0.0" if is_prod else "127.0.0.1",
+    backend_host="0.0.0.0",
     backend_port=int(os.getenv("PORT", 8000)),
-    api_url=os.getenv("API_URL", "http://127.0.0.1:8000"),
-    plugins=[
-        rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
-    ]
 )
