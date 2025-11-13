e2b1e485402b3a5791220d0ff1107770a8e54a8f
Huy Pham <124356254+HuyPham171-hub@users.noreply.github.com>
2025-11-13 18:00:25 +0700

    Disable SitemapPlugin to reduce memory usage

--- rxconfig.py
+++ rxconfig.py
@@ -5,4 +5,5 @@ config = rx.Config(
     app_name="goldsight",
     backend_host="0.0.0.0",
     backend_port=int(os.getenv("PORT", 8000)),
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
